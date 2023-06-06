5fda28373a5cb01c2d9c6688b1175b9ed59a908e
MrNedNick <mrnediala@gmail.com>
2023-06-06 15:50:57 +0200

    Add style for todo page

--- src/components/TodoPage/TodoPage.jsx
+++ src/components/TodoPage/TodoPage.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useLocation } from "react-router";
+import s from './TodoPage.module.css';
+import TodoItem from '../TodoItem/TodoItem';
 
 const TodoPage = () => {
   const location = useLocation();
   const { state } = location;
   console.log(state);
   return (
-    <div>
-      <NavLink to='/'>Back</NavLink>
-      <div>
-        {state.todo.text}
-      </div>
-      <div>
-        {state.todo.id}
-      </div>
+    <div className={s.container}>
+      <NavLink className={s.navLink} to='/'>Back to Main Page</NavLink>
+      <TodoItem todo={state.todo} />
+      {/* <div className={s.contentContainer}>
+        <span>Todo: {state.todo.text}</span>
+        <span>Todo id:{state.todo.id}</span>
+      </div> */}
     </div>
   );
 };
